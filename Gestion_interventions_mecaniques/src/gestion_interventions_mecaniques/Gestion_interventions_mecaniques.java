7e4563ceb7ae254c5ea1f9d46ea4779f4b5e1359
gitgeto10 <oumaimaaitelbourane@gmail.com>
2024-05-01 19:23:20 +0200

    Ajout du contenu du dossier Gestion_interventions_mecaniques

---
+++ Gestion_interventions_mecaniques/src/gestion_interventions_mecaniques/Gestion_interventions_mecaniques.java
@@ -0,0 +1,14 @@
+
+package gestion_interventions_mecaniques;
+
+import ologin.LoginFrame;
+
+
+public class Gestion_interventions_mecaniques {
+
+    public static void main(String[] args) {
+        LoginFrame obj = new LoginFrame();
+        obj.show();
+    }
+    
+}
